e1ede00c2ba49a81a54a6bc4d6ba4b549391e010
Elia <emrahkrkmz1@gmail.com>
2017-08-20 04:09:08 +0300

    Updated

--- iCard/MainViewController.swift
+++ iCard/MainViewController.swift
@@ -12,8 +12,8 @@ import SnapKit
 class MainViewController: UIViewController{
     
     fileprivate func test2(){
-        let cardTest1: iCreditCard = iCreditCard(cardStyleWithBackgroundColor: .withBothBankIcon, withCardColors: .yelloway, pinIcon: .chip2, creditIcons: .googleWalletIcon)
-        let cardTest2: iCreditCard = iCreditCard(cardStyleWithBackgroundColor: .withBothBankIcon, withCardColors: .blueway, pinIcon: .chip1, creditIcons: .cirrusIcon)
+        let cardTest1: iCreditCard = iCreditCard(cardStyleWithBackgroundColor: .withBothBankIcon, withCardColors: .blueway, pinIcon: .chip2, creditIcons: .masterCard)
+        let cardTest2: iCreditCard = iCreditCard(cardStyleWithBackgroundColor: .withBothBankIcon, withCardColors: .yelloway, pinIcon: .chip1, creditIcons: .cirrusIcon)
         let cardTest3: iCreditCard = iCreditCard(cardStyleWithBackgroundImage: .withBothBankIcon, withCardImage: UIImage(named:"simpleBackgroundImage"), pinIcon: .chip2, creditIcons: .maestroIcon)
          
         view.addSubview(cardTest1)
